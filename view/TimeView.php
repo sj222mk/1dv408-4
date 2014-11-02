669e63ee8474e3104086fe62b1b33aa252c7d1bb
sj222mk <sj222mk@student.lnu.se>
2014-11-02 15:06:20 +0100

    datumfix

--- view/TimeView.php
+++ view/TimeView.php
@@ -48,40 +48,40 @@ class TimeView{
 	
 	function setMonth($m){
 		switch($m){ 
-        case '01': 
+        case '1': 
             $m = 'Januari'; 
         break; 
-        case '1': 
+        case '2': 
             $m = 'Februari'; 
         break; 
-        case '2': 
+        case '3': 
             $m = 'Mars'; 
 			break;
-		case '3': 
+		case '4': 
             $m = 'April'; 
         break; 
-		case '4': 
+		case '5': 
             $m = 'Maj'; 
         break; 
-		case '5': 
+		case '6': 
             $m = 'Juni'; 
         break; 
-		case '6': 
+		case '7': 
             $m = 'Juli'; 
         break; 
-		case '7': 
+		case '8': 
             $m = 'Augusti'; 
 			break;
-		case '8': 
+		case '9': 
             $m = 'September'; 
         break; 
-		case '9': 
+		case '10': 
             $m = 'Oktober'; 
         break; 
-		case '10': 
+		case '11': 
             $m = 'November'; 
         break; 
-		case '11': 
+		case '12': 
             $m = 'December'; 
         break; 
     	} 
